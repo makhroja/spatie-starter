7ccd0321aa1309e912174aaf3016b27aadb154c4
Djendra Priono <djendrapriono@hotmail.com>
2023-02-13 03:18:11 +0700

    Halaman Role Spatie

---
+++ app/Http/Controllers/Settings/RoleController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers\Settings;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\RoleRequest;
+use Illuminate\Http\Request;
+use Spatie\Permission\Models\Role;
+use Yajra\DataTables\DataTables;
+
+class RoleController extends Controller
+{
+    public function index(Request $request)
+    {
+        if ($request->ajax()) {
+            $query = Role::latest()->get();
+
+            return DataTables::of($query)
+                ->addIndexColumn()
+                ->addColumn('action', function ($row) {
+                    $btn = '<button href="javascript:void(0)" data-id="' . $row->id . '" data-original-title="Show" class="btn btn-outline-primary btn-icon show"><i class="feather icon-eye"></i></button>';
+
+                    $btn = $btn . ' <button href="javascript:void(0)" data-id="' . $row->id . '" data-original-title="Edit" class="btn btn-outline-success btn-icon edit"><i class="feather icon-edit"></i></button>';
+
+                    $btn = $btn . ' <button href="javascript:void(0)" data-name="' . $row->name . '" data-id="' . $row->id . '" data-original-title="Delete" class="btn btn-outline-danger btn-icon delete"><i class="feather icon-trash"></i></button>';
+
+                    return $btn;
+                })
+                ->rawColumns(['action'])
+                ->make(true);
+        }
+
+        return view('settings.role.index');
+    }
+
+    public function createRole(RoleRequest $request)
+    {
+        $request->merge([
+            'guard_name' => 'web'
+        ]);
+        Role::create($request->all());
+        return back()
+            ->withSuccess('Role baru berhasil disimpan')
+            ->withInput();
+    }
+
+    public function deleteRole(Request $request)
+    {
+        $query = Role::findOrFail($request->id);
+        $query->delete();
+
+        return response()->json([
+            'status' => 200,
+            'success' => 'Role berhasil dihapus',
+        ]);
+    }
+
+    public function showRole(Request $request)
+    {
+        $query = Role::findOrFail($request->id);
+
+        return response()->json([
+            'status' => 200,
+            'role' => $query,
+            'success' => 'Role berhasil diambil',
+        ]);
+    }
+
+    public function editRole(Request $request)
+    {
+        $query = Role::findOrFail($request->id);
+
+        return response()->json([
+            'status' => 200,
+            'role' => $query,
+            'success' => 'Role berhasil diambil',
+        ]);
+    }
+
+    public function updateRole(RoleRequest $request)
+    {
+        $query = Role::findOrFail($request->id);
+        $query->update($request->all());
+
+        return redirect()->route('indexRole')
+            ->withSuccess('Role berhasil diubah')
+            ->withInput();
+    }
+}
---
+++ resources/views/settings/role/index.blade.php
@@ -0,0 +1,265 @@
+@extends('layout.master')
+
+@push('plugin-styles')
+    <link href="{{ asset('assets/plugins/datatables-net/dataTables.bootstrap4.css') }}" rel="stylesheet" />
+@endpush
+
+@section('content')
+    <nav class="page-breadcrumb">
+
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="#">Dashboard</a></li>
+            <li class="breadcrumb-item active" aria-current="page">Role</li>
+        </ol>
+    </nav>
+    <div class="row">
+        <div class="col-md-12 grid-margin stretch-card">
+            <div class="card">
+                <div class="card-body">
+                    <h6 class="card-title">Role</h6>
+                    <button type="button" class="btn btn-icon btn-outline-primary create">
+                        <i data-feather="plus-square"></i>
+                    </button>
+                    <hr>
+                    <div class="table-responsive">
+                        <table id="dataTable" class="table table-bordered table-hover">
+                            <thead>
+                                <tr>
+                                    <th>No</th>
+                                    <th>Team</th>
+                                    <th>Name</th>
+                                    <th>Guard</th>
+                                    <th></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+
+@push('plugin-scripts')
+    <script src="{{ asset('assets/plugins/datatables-net/jquery.dataTables.js') }}"></script>
+    <script src="{{ asset('assets/plugins/datatables-net-bs4/dataTables.bootstrap4.js') }}"></script>
+@endpush
+
+@push('custom-scripts')
+    <script type="text/javascript">
+        $(function() {
+
+            // datatable
+            let table = $('#dataTable').DataTable({
+                processing: true,
+                serverSide: true,
+                scrollY: true,
+                scrollX: true,
+                // select: true,
+                // select: 'single',
+                ajax: "{{ route('indexRole') }}",
+                columnDefs: [{
+                    targets: "_all",
+                    orderable: false
+                }, {
+                    width: "2%",
+                    targets: [0, 1, 3, 4]
+                }, {
+                    //buat wrap text
+                    render: function(data, type, full, meta) {
+                        return "<div class='text-wrap width-200'>" + data + "</div>";
+                    },
+                    targets: [1]
+                }, ],
+                columns: [{
+                        data: 'DT_RowIndex',
+                        name: 'DT_RowIndex'
+                    },
+                    {
+                        data: 'team_id',
+                        name: 'team_id'
+                    },
+                    {
+                        data: 'name',
+                        name: 'name'
+                    },
+                    {
+                        data: 'guard_name',
+                        name: 'guard_name'
+                    },
+                    {
+                        data: 'action',
+                        name: 'action',
+                        orderable: false,
+                        searchable: false
+                    },
+                ],
+            });
+
+            // for create
+            $('.create').on('click', function() {
+                alertify.alert().set({
+                    title: 'Role Create',
+                    label: 'Batal',
+                    message: `
+                        <form action="{{ route('createRole') }}" method="POST">
+                        @csrf
+                        <div class="modal-content">
+                        <div class="modal-body">
+                        <div class="form-group mb-3">
+                        <label for="">Team Id</label>
+                        <input name="team_id" type="text" required class="form-control">
+                        </div>
+                        <div class="form-group mb-3">
+                        <label for="">Role Name</label>
+                        <input name="name" type="text" required class="form-control">
+                        </div>
+                        </div>
+                        <div class="modal-footer bg-white">
+                        <button type="submit" class="btn btn-primary">Simpan</button>
+                        </div>
+                        </div>
+                        </form>
+                        `
+                }).show();
+            })
+
+            // for edit
+            table.on('click', '.edit', function() {
+                let id = $(this).data("id");
+                $.ajax({
+                    url: "{{ route('editRole') }}",
+                    type: 'GET',
+                    dataType: "JSON",
+                    data: {
+                        "id": id,
+                    },
+                    success: function(data) {
+                        table.draw();
+                        console.log('Success:', data);
+                        alertify.alert().set({
+                            title: 'Role Edit',
+                            label: 'Batal',
+                            message: `
+                                <form action="{{ route('updateRole') }}" method="POST">
+                                @csrf
+                                <div class="modal-content">
+                                <div class="modal-body">
+                                <input type="hidden" name="id" value="` + data.role.id + `" />
+                                <div class="form-group mb-3">
+                                <label for="">Team</label>
+                                <input name="team_id" type="text" required class="form-control" value="` + data.role
+                                .team_id + `">
+                                </div>
+                                <div class="form-group mb-3">
+                                <label for="">Role Name</label>
+                                <input name="name" type="text" required class="form-control" value="` + data.role
+                                .name + `">
+                                </div>
+                                </div>
+                                <div class="modal-footer bg-white">
+                                <button type="submit" class="btn btn-primary">Simpan</button>
+                                </div>
+                                </div>
+                                </form>
+                                `
+                        }).show();
+                    },
+                    error: function(data) {
+                        console.log('Error:', data);
+                        alertify.alert().set({
+                            title: 'Error',
+                            message: data.responseJSON.message
+                        }).show();
+                    }
+                });
+            });
+
+            // for read
+            table.on('click', '.show', function() {
+                let id = $(this).data("id");
+
+                $.ajax({
+                    url: "{{ route('showRole') }}",
+                    type: 'GET',
+                    dataType: "JSON",
+                    data: {
+                        "id": id,
+                    },
+                    success: function(data) {
+                        table.draw();
+                        console.log('Success:', data);
+                        alertify.alert().set({
+                            title: 'Role Detail',
+                            transition: 'zoom',
+                            message: '<table class="table table-bordered">' +
+                                '<tr>' +
+                                '<th style="width:10%">Team</th>' +
+                                '<td>' + data.role.team_id + '</td>' +
+                                '</tr>' +
+                                '<tr>' +
+                                '<th style="width:10%">Name</th>' +
+                                '<td>' + data.role.name + '</td>' +
+                                '</tr>' +
+                                '<tr>' +
+                                '<th style="width:10%">Guard</th>' +
+                                '<td>' + data.role.guard_name + '</td>' +
+                                '</tr>' +
+                                '</table>'
+                        }).show();
+                    },
+                    error: function(data) {
+                        console.log('Error:', data);
+                        alertify.alert().set({
+                            'title': 'Error',
+                            'message': data.responseJSON.message
+                        }).show();
+                    }
+                });
+            });
+
+            // for delete
+            table.on('click', '.delete', function() {
+                let name = $(this).data("name");
+                let id = $(this).data("id");
+                alertify.confirm('Hapus Role', 'Anda yakin akan menghapus role <b>' + name + '</b>',
+                    function() {
+
+                        $.ajax({
+                            url: "{{ route('deleteRole') }}",
+                            type: 'DELETE',
+                            dataType: "JSON",
+                            data: {
+                                "id": id,
+                                "_token": '{{ csrf_token() }}',
+                            },
+                            success: function(data) {
+                                table.draw();
+                                console.log('Success:', data);
+                                alertify.alert().set({
+                                    'title': 'Success',
+                                    'message': data.success
+                                }).show();
+                            },
+                            error: function(data) {
+                                console.log('Error:', data);
+                                alertify.alert().set({
+                                    'title': 'Error',
+                                    'message': data.responseJSON.message
+                                }).show();
+                            }
+                        });
+                    },
+                    function() {
+                        alertify.error('Cancel')
+                    });
+            });
+
+            /*
+             *End Document Ready
+             */
+        });
+    </script>
+@endpush
