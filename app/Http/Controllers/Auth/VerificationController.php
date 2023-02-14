c40c429d1cd99c11db8c816daeea03e9cf467020
Djendra Priono <djendrapriono@hotmail.com>
2023-02-14 23:49:54 +0700

    Redirect ke GATE

--- app/Http/Controllers/Auth/VerificationController.php
+++ app/Http/Controllers/Auth/VerificationController.php
@@ -26,7 +26,7 @@ class VerificationController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
+    protected $redirectTo = RouteServiceProvider::GATE;
 
     /**
      * Create a new controller instance.
